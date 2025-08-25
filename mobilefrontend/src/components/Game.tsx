c93e7ea6f18f8482d07d77b50263a2837290c1dd
Tim Arnold <tim-arnold@outlook.de>
2025-08-25 13:32:54 +0200

    fixed url

--- mobilefrontend/src/components/Game.tsx
+++ mobilefrontend/src/components/Game.tsx
@@ -24,7 +24,7 @@ export default function Game(props: { sessionID: string, indoor: boolean }) {
     const {
         sendJsonMessage,
         readyState
-    } = useWebSocket("http://localhost:3000/play?sessionID=" + props.sessionID, {
+    } = useWebSocket("https://rv6l.tim-arnold.de/play?sessionID=" + props.sessionID, {
         onOpen: () => console.log('opened'),
         onMessage: (message) => {
             console.log('message received', message);
