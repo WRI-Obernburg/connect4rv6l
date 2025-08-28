dbe0bf310ee4674abcd0acb588e2623d924b596d
Tim Arnold <tim-arnold@outlook.de>
2025-08-28 11:55:11 +0200

    Enable trailing slashes and skip trailing slash redirects in Next.js configuration

--- controlpanel/next.config.ts
+++ controlpanel/next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
     output: 'export',
     distDir: 'dist',
+    trailingSlash: true,
+    skipTrailingSlashRedirect: true,
     basePath: "/control"
 };
 
